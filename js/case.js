568a0c4d8f4f950aa7a20584da9c6ac005d69977
Sumankdatta <sumankdatta18@gmail.com>
2023-09-27 19:29:02 +0600

    first commit added

---
+++ js/case.js
@@ -0,0 +1,39 @@
+function updateCaseNumber(isIncrease) {
+    const caseNumberField = document.getElementById('case-number-field')
+    const caseNumberString = caseNumberField.value;
+    const caseNumber = parseInt(caseNumberString);
+
+    let newCaseNumber
+
+    if (isIncrease === true) {
+        newCaseNumber = caseNumber + 1;
+    }
+    else {
+        newCaseNumber = caseNumber - 1;
+    }
+    if (newCaseNumber < 1) {
+        alert('Can not negative')
+    }
+    else {
+        caseNumberField.value = newCaseNumber;
+    }
+
+    return newCaseNumber;
+
+}
+
+function updateCasePrice(caseNumber) {
+    const totalAmount = caseNumber * 59;
+    const casePrice = document.getElementById('case-price')
+    casePrice.innerText = totalAmount
+}
+
+document.getElementById("case-btn-plus").addEventListener('click', function () {
+    const caseNumber = updateCaseNumber(true);
+    updateCasePrice(caseNumber)
+
+})
+document.getElementById("case-btn-minus").addEventListener('click', function () {
+    const caseNumber = updateCaseNumber(false)
+    updateCasePrice(caseNumber)
+})
